bc0fc13c0fcfbf034f26cb549d9bb4e9249d55f5
Vetle Økland <vetle.okland@nagarro.com>
2019-11-14 08:56:48 +0100

    Initial Commit

---
+++ AirControl/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AirControl
+//
+//  Created by Vetle Økland on 14/11/2019.
+//  Copyright © 2019 Vetle Økland. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
